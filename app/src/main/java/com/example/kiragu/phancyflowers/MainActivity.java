e4c2fe394327546d2cee0859e4c1e09ffb411c84
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-20 15:04:28 +0300

    Added onclick listeners on the main activity

--- app/src/main/java/com/example/kiragu/phancyflowers/MainActivity.java
+++ app/src/main/java/com/example/kiragu/phancyflowers/MainActivity.java
@@ -1,20 +1,45 @@
 package com.example.kiragu.phancyflowers;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.kiragu.phancyflowers.ui.LoginActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginButton)Button mLoginButton;
+    @Bind(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         VideoView videoview = (VideoView) findViewById(R.id.videoView);
         Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg);
         videoview.setVideoURI(uri);
         videoview.start();
+
+        mLoginButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+        if (v == mSignUpButton) {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
     }
 }
