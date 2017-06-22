ed7a262c2a264ef5b10e3d12e57d8981ad736604
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-22 16:32:29 +0300

    Authenticated Login activity with firebase

--- app/src/main/java/com/example/kiragu/phancyflowers/ui/LoginActivity.java
+++ app/src/main/java/com/example/kiragu/phancyflowers/ui/LoginActivity.java
@@ -2,15 +2,22 @@ package com.example.kiragu.phancyflowers.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kiragu.phancyflowers.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,18 +28,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button mLoginButton1;
     @Bind(R.id.loginTextView)
     TextView mLoginTextView;
-    @Bind(R.id.emailTextView)
+    @Bind(R.id.emailEditView1)
     EditText mEmailView;
     @Bind(R.id.passwordEditText) EditText mPassword;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+
         mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, ProductListActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
 
         mLoginButton1.setOnClickListener(this);
         mLoginTextView.setOnClickListener(this);
@@ -58,6 +80,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (password.equals("")) {
             mPassword.setError("Password cannot be blank");
             return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Please, Try Again?",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }
