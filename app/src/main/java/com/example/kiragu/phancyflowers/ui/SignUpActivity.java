9e8cc942d2d6181cabceb3d2456d267379b68fab
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-22 09:30:11 +0300

    Added a google_services.json to connect phancy petals with firebase

--- app/src/main/java/com/example/kiragu/phancyflowers/ui/SignUpActivity.java
+++ app/src/main/java/com/example/kiragu/phancyflowers/ui/SignUpActivity.java
@@ -1,15 +1,35 @@
 package com.example.kiragu.phancyflowers.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.kiragu.phancyflowers.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+@Bind(R.id.signUpTextView)
+    TextView mSignUpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mSignUpTextView.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v){
+        if(v == mSignUpTextView){
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
